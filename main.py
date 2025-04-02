f1b72246aa88301b28b2bbb2448d8c4dc632c7f1
Arif Choudhary <choudharyarif756@gmail.com>
2025-04-02 23:25:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def listAllExpenses():
 
 def viewExpenseInfo():
     global data_table
-    global dateField, payee, description, amount, modeOfPayment
+    global dateField, payee, description, amount, modeOfPayment, category
     if not data_table.selection():
         mb.showerror('No expense selected', 'Please select an expense from the table to view its details')
         return
@@ -49,15 +49,17 @@ def viewExpenseInfo():
     description.set(val[3])
     amount.set(val[4])
     modeOfPayment.set(val[5])
+    category.set(val[6])
 
 
 def clearFields():
-    global description, payee, amount, modeOfPayment, dateField, data_table
+    global description, payee, amount, modeOfPayment, category, dateField, data_table
     todayDate = datetime.datetime.now().date()
     description.set('')
     payee.set('')
     amount.set(0.0)
     modeOfPayment.set('Cash')
+    category.set('Food')
     dateField.set_date(todayDate)
     data_table.selection_remove(*data_table.selection())
 
@@ -93,8 +95,8 @@ def removeAllExpenses():
 
 
 def addAnotherExpense():
-    global dateField, payee, description, amount, modeOfPayment, dbconnector
-    if not dateField.get() or not payee.get() or not description.get() or not amount.get() or not modeOfPayment.get():
+    global dateField, payee, description, amount, modeOfPayment, category, dbconnector
+    if not dateField.get() or not payee.get() or not description.get() or not amount.get() or not modeOfPayment.get() or not category.get():
         mb.showerror('Fields empty!', "Please fill all the missing fields before pressing the add button!")
         return
     try:
@@ -106,8 +108,8 @@ def addAnotherExpense():
         mb.showerror('Invalid Amount', 'Please enter a positive amount')
         return
     dbconnector.execute(
-        'INSERT INTO ExpenseTracker (Date, Payee, Description, Amount, ModeOfPayment) VALUES (?, ?, ?, ?, ?)',
-        (dateField.get_date(), payee.get(), description.get(), amount.get(), modeOfPayment.get())
+        'INSERT INTO ExpenseTracker (Date, Payee, Description, Amount, ModeOfPayment, Category) VALUES (?, ?, ?, ?, ?, ?)',
+        (dateField.get_date(), payee.get(), description.get(), amount.get(), modeOfPayment.get(), category.get())
     )
     dbconnector.commit()
     clearFields()
@@ -117,13 +119,13 @@ def addAnotherExpense():
 
 def editExpense():
     def editExistingExpense():
-        global dateField, amount, description, payee, modeOfPayment
+        global dateField, amount, description, payee, modeOfPayment, category
         global dbconnector, data_table
         currentSelectedExpense = data_table.item(data_table.focus())
         content = currentSelectedExpense['values']
         dbconnector.execute(
-            'UPDATE ExpenseTracker SET Date = ?, Payee = ?, Description = ?, Amount = ?, ModeOfPayment = ? WHERE ID = ?',
-            (dateField.get_date(), payee.get(), description.get(), amount.get(), modeOfPayment.get(), content[0])
+            'UPDATE ExpenseTracker SET Date = ?, Payee = ?, Description = ?, Amount = ?, ModeOfPayment = ?, Category = ? WHERE ID = ?',
+            (dateField.get_date(), payee.get(), description.get(), amount.get(), modeOfPayment.get(), category.get(), content[0])
         )
         dbconnector.commit()
         clearFields()
@@ -158,16 +160,16 @@ def selectedExpenseToWords():
         return
     currentSelectedExpense = data_table.item(data_table.focus())
     val = currentSelectedExpense['values']
-    msg = f'Your expense can be read like: \n"You paid {val[4]} to {val[2]} for {val[3]} on {val[1]} via {val[5]}"'
+    msg = f'Your expense can be read like: \n"You paid {val[4]} to {val[2]} for {val[3]} on {val[1]} via {val[5]} in category {val[6]}"'
     mb.showinfo('Here\'s how to read your expense', msg)
 
 
 def expenseToWordsBeforeAdding():
-    global dateField, description, amount, payee, modeOfPayment
+    global dateField, description, amount, payee, modeOfPayment, category
 
     msg = (f'Your expense can be read like: \n"You paid {amount.get()} to {payee.get()} '
            f'for {description.get()} on '
-           f'{dateField.get()} via {modeOfPayment.get()}"')
+           f'{dateField.get()} via {modeOfPayment.get()} in category {category.get()}"')
     mb.showinfo('Here\'s how to read your expense', msg)
 
 
@@ -176,7 +178,7 @@ def exportExpenses():
     all_data = dbconnector.execute('SELECT * FROM ExpenseTracker')
     data = all_data.fetchall()
 
-    df = pd.DataFrame(data, columns=['ID', 'Date', 'Payee', 'Description', 'Amount', 'ModeOfPayment'])
+    df = pd.DataFrame(data, columns=['ID', 'Date', 'Payee', 'Description', 'Amount', 'ModeOfPayment', 'Category'])
     
     file_path = filedialog.asksaveasfilename(defaultextension='.xlsx', filetypes=[('Excel files', '*.xlsx')])
     
@@ -234,6 +236,18 @@ def displayGraph():
         ax.bar(labels, values, color='salmon')
         ax.set_xlabel('Month')
         ax.set_ylabel('Total Amount Spent')
+    
+    elif graphOption.get() == "Total Amount Spent per Category":
+        category_amount = {}
+        for row in data:
+            if row[6] not in category_amount:
+                category_amount[row[6]] = 0
+            category_amount[row[6]] += row[4]
+        labels = category_amount.keys()
+        values = category_amount.values()
+        ax.bar(labels, values, color='purple')
+        ax.set_xlabel('Category')
+        ax.set_ylabel('Total Amount Spent')
 
     ax.set_title('Amount Spent Graph')
     plt.xticks(rotation=45)
@@ -258,9 +272,9 @@ def saveGraph(fig):
 def searchExpenses():
     keyword = searchEntry.get()
     query = ("SELECT * FROM ExpenseTracker WHERE Date LIKE ? OR Payee LIKE ? OR Description LIKE ? OR Amount LIKE ? OR "
-             "ModeOfPayment LIKE ?")
+             "ModeOfPayment LIKE ? OR Category LIKE ?")
     data = dbconnector.execute(query, (
-        '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%'))
+        '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%', '%' + keyword + '%'))
     data_table.delete(*data_table.get_children())
     for val in data:
         data_table.insert('', END, values=val)
@@ -269,7 +283,7 @@ def searchExpenses():
 def showTotalExpense():
     global dbconnector
     total = dbconnector.execute('SELECT SUM(Amount) FROM ExpenseTracker').fetchone()[0]
-    total = total if total else 0  # Handle None case
+    total = total if total else 0
     mb.showinfo('Total Expense', f'The total expense is: {total}')
 
 
@@ -277,14 +291,15 @@ def showMonthlyExpense():
     global dbconnector
     current_month = datetime.datetime.now().strftime("%Y-%m")
     total = dbconnector.execute('SELECT SUM(Amount) FROM ExpenseTracker WHERE Date LIKE ?', (f'{current_month}%',)).fetchone()[0]
-    total = total if total else 0  # Handle None case
+    total = total if total else 0
     mb.showinfo('Monthly Expense', f'The total expense for this month is: {total}')
+
 
 def showYearlyExpense():
     global dbconnector
     current_year = datetime.datetime.now().strftime("%Y")
     total = dbconnector.execute('SELECT SUM(Amount) FROM ExpenseTracker WHERE Date LIKE ?', (f'{current_year}%',)).fetchone()[0]
-    total = total if total else 0  # Handle None case
+    total = total if total else 0
     mb.showinfo('Yearly Expense', f'The total expense for this year is: {total}')
 
 
@@ -296,7 +311,7 @@ style = ttk.Style()
 style.configure('TButton', font=('Bahnschrift Condensed', 13), background='#FFA07A', foreground='#FFFFFF',
                 relief=GROOVE, padding=5)
 style.configure('TLabel', font=('Bahnschrift Condensed', 15), foreground='#333333')
-style.configure('Treeview.Heading', font=('Bahnschrift Condensed', 15), background='#FFA07A', foreground='#FFFFFF')
+style.configure('Treeview.Heading', font=('Bahnschrift Condensed', 15), background='#FFA07A', foreground='#000000')
 style.configure('Treeview', font=('Bahnschrift Condensed', 13), rowheight=25)
 
 titleLabel = Label(mainWindow, text="Expense Tracker", font=("Bahnschrift Condensed", 24), bg="#FFA07A", fg="#FFFFFF")
@@ -329,21 +344,22 @@ dbconnector.execute('''CREATE TABLE IF NOT EXISTS ExpenseTracker (
                        Payee TEXT NOT NULL,
                        Description TEXT NOT NULL,
                        Amount REAL NOT NULL,
-                       ModeOfPayment TEXT NOT NULL
+                       ModeOfPayment TEXT NOT NULL,
+                       Category TEXT NOT NULL
                    )''')
 dbconnector.commit()
 
-data_table = ttk.Treeview(mainFrame, columns=('ID', 'Date', 'Payee', 'Description', 'Amount', 'ModeOfPayment'),
+data_table = ttk.Treeview(mainFrame, columns=('ID', 'Date', 'Payee', 'Description', 'Amount', 'ModeOfPayment', 'Category'),
                           show='headings', style='Treeview')
-for col in ('ID', 'Date', 'Payee', 'Description', 'Amount', 'ModeOfPayment'):
+for col in ('ID', 'Date', 'Payee', 'Description', 'Amount', 'ModeOfPayment', 'Category'):
     data_table.heading(col, text=col)
 data_table.pack(fill=BOTH, expand=True)
 
 frameL1 = Frame(mainWindow, padx=10, pady=10, bg="#F5F5F5")
 frameL1.pack(side=LEFT, anchor=NW)
 
-Label(frameL1, text="Date:", font=("Bahnschrift Condensed", "15"), bg="#F5F5F5", fg="#333333").grid(row=1, column=0, sticky=W, padx=50, pady=10)
-dateField = DateEntry(frameL1, width=20, background='#4CAF50', foreground='white', borderwidth=2)
+Label(frameL1, text="Date:", font=("Bahnschrift Condensed", "15"), bg="#F5F5F5", fg="#000000").grid(row=1, column=0, sticky=W, padx=50, pady=10)
+dateField = DateEntry(frameL1, width=20, background='#4CAF50', foreground='black', borderwidth=2)
 dateField.grid(row=1, column=1, sticky=W, padx=10, pady=10)
 
 Label(frameL1, text="Payee:", font=("Bahnschrift Condensed", "15"), bg="#F5F5F5", fg="#333333").grid(row=2, column=0, sticky=W, padx=50, pady=10)
@@ -369,25 +385,31 @@ paymentOptions = ['Cash', 'Credit Card', 'Debit Card', 'Net Banking', 'UPI', 'Ot
 modeOfPayment.set('Cash')
 option_menu = OptionMenu(frameL1, modeOfPayment, *paymentOptions)
 option_menu.grid(row=5, column=1, sticky=W, padx=10, pady=10)
-
 option_menu.config(width=18)
 
+Label(frameL1, text="Category:", font=("Bahnschrift Condensed", "15"), bg="#F5F5F5", fg="#333333").grid(row=6, column=0, sticky=W, padx=50, pady=10)
+category = StringVar()
+categoryOptions = ['Food', 'Groceries', 'Bills', 'Transportation', 'Entertainment', 'Education', 'Health', 'Shopping', 'Housing', 'Others']
+category.set('Food')
+category_menu = OptionMenu(frameL1, category, *categoryOptions)
+category_menu.grid(row=6, column=1, sticky=W, padx=10, pady=10)
+category_menu.config(width=18)
+
 Button(frameL1, text="Clear Fields", font=("Bahnschrift Condensed", "13"), width=20, bg="#FFA07A", fg="#FFFFFF",
-       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=clearFields).grid(row=6,
+       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=clearFields).grid(row=7,
                                                                                                         column=0,
                                                                                                         sticky=W,
                                                                                                         padx=50,
                                                                                                         pady=10)
 
 Button(frameL1, text="Add", font=("Bahnschrift Condensed", "13"), width=20, bg="#FFA07A", fg="#FFFFFF", relief=GROOVE,
-       activebackground="#FF7F50", activeforeground="#FFFFFF", command=addAnotherExpense).grid(row=6, column=1,
+       activebackground="#FF7F50", activeforeground="#FFFFFF", command=addAnotherExpense).grid(row=7, column=1,
                                                                                                sticky=W, padx=10,
                                                                                                pady=10)
 frameL2 = Frame(mainWindow, padx=10, pady=10, bg="#F5F5F5")
 frameL2.pack(side=LEFT, anchor=NW)
 
 listAllExpenses()
-
 
 Button(frameL2, text="View Expense Info", font=("Bahnschrift Condensed", "13"), width=30, bg="#FFA07A", fg="#FFFFFF",
        relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=viewExpenseInfo).grid(row=1,
@@ -435,6 +457,21 @@ Button(frameL3, text="Export to Excel", font=("Bahnschrift Condensed", "13"), wi
                                                                                                            padx=50,
                                                                                                            pady=10)
 
+graphFrame = Frame(frameL3, bg="#F5F5F5")
+graphFrame.grid(row=2, column=0, sticky=W, padx=50, pady=10)
+
+Label(graphFrame, text="Select Graph Type:", font=("Bahnschrift Condensed", "13"), bg="#F5F5F5", fg="#333333").pack(side=LEFT, padx=5)
+
+graphOption = StringVar()
+graphOption.set("Total Amount Spent per Mode of Payment")
+graph_option_menu = OptionMenu(graphFrame, graphOption,
+                               "Total Amount Spent per Mode of Payment",
+                               "Total Amount Spent per Payee",
+                               "Total Amount Spent per Month",
+                               "Total Amount Spent per Category")
+graph_option_menu.pack(side=LEFT)
+graph_option_menu.config(width=25)
+
 Button(frameL3, text="View Graph", font=("Bahnschrift Condensed", "13"), width=30, bg="#FFA07A", fg="#FFFFFF",
        relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=displayGraph).grid(row=3,
                                                                                                          column=0,
@@ -442,39 +479,29 @@ Button(frameL3, text="View Graph", font=("Bahnschrift Condensed", "13"), width=3
                                                                                                          padx=50,
                                                                                                          pady=10)
 
-graphOption = StringVar()
-graphOption.set("Total Amount Spent per Mode of Payment")
-graph_option_menu = OptionMenu(frameL3, graphOption,
-                               "Total Amount Spent per Mode of Payment",
-                               "Total Amount Spent per Payee",
-                               "Total Amount Spent per Month")
-graph_option_menu.grid(row=2, column=0, sticky=W, padx=50, pady=10)
-
-graph_option_menu.config(width=29)
-
 Button(frameL3, text="Show Total Expense", font=("Bahnschrift Condensed", "13"), width=30, bg="#FFA07A", fg="#FFFFFF",
-       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=showTotalExpense).grid(row=5,
+       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=showTotalExpense).grid(row=4,
                                                                                                              column=0,
                                                                                                              sticky=W,
                                                                                                              padx=50,
                                                                                                              pady=10)
 
 Button(frameL3, text="Show Monthly Expense", font=("Bahnschrift Condensed", "13"), width=30, bg="#FFA07A", fg="#FFFFFF",
-       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=showMonthlyExpense).grid(row=6,
+       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=showMonthlyExpense).grid(row=5,
                                                                                                               column=0,
                                                                                                               sticky=W,
                                                                                                               padx=50,
                                                                                                               pady=10)
 
 Button(frameL3, text="Show Yearly Expense", font=("Bahnschrift Condensed", "13"), width=30, bg="#FFA07A", fg="#FFFFFF",
-       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=showYearlyExpense).grid(row=7,
+       relief=GROOVE, activebackground="#FF7F50", activeforeground="#FFFFFF", command=showYearlyExpense).grid(row=6,
                                                                                                              column=0,
                                                                                                              sticky=W,
                                                                                                              padx=50,
                                                                                                              pady=10)
 
 Button(frameL3, text="Exit", font=("Bahnschrift Condensed", "13"), width=30, bg="#FFA07A", fg="#FFFFFF", relief=GROOVE,
-       activebackground="#FF7F50", activeforeground="#FFFFFF", command=mainWindow.quit).grid(row=4,
+       activebackground="#FF7F50", activeforeground="#FFFFFF", command=mainWindow.quit).grid(row=7,
                                                                                              column=0,
                                                                                              sticky=W,
                                                                                              padx=50,
